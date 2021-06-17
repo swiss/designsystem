01d6e695c958fb06934e545f5ee7b0b47785838e
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-06-17 18:35:02 +0200

    BDS-41 make vue app work

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>I must be tall and red</h1>
-  <Logo title="Design System for the Swiss Confederation" accronym="DSS" />
-  <button>Button</button>
+  <div>
+    <h1 class="mb-12 3xl">Title</h1>
+    <Logo title="Design System for the Swiss Confederation" accronym="DSS" />
+  </div>
 </template>
 
-<script setup>
-import Logo from './components/components/logo/logo.vue'
+<script>
+import Logo from './components/logo/logo.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+  name: 'App',
+  components: {
+    Logo
+  }
+}
 </script>
 
