291117913c1c30b280eef4588188b001e7ad0994
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-06-29 15:20:17 +0200

    BDS-42 Add mdx story, add storybook-addon-html, reorder stories

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -5,6 +5,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    // "@storybook/addon-a11y",
+    "@whitespace/storybook-addon-html"
   ]
 }
