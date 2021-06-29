de3f99ce27be0349d9c804f094655339162eb40b
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-06-29 12:30:44 +0200

    BDS-93 add addons and configure nuxt-sb

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -58,10 +58,37 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      presets(_, [preset, options]) {
+        options.corejs = { version: 3 }
+      },
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }],
+        ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+      ],
+    },
   },
 
   // Storybook for nuxt project: https://storybook.nuxtjs.org/setup#installation
   storybook: {
-    // Options
-  }
+    addons: [
+      '@storybook/addon-essentials',
+      '@storybook/addon-a11y',
+      '@whitespace/storybook-addon-html'
+    ],
+    stories: [
+      '~/components/stories/**/*.stories.mdx',
+      '~/components/stories/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    parameters: {
+      options: {
+        storySort: {
+          order: ['Introduction', 'Foundations', ['Overview', 'Colors', 'Icons'], 'Components', 'Examples'],
+        },
+      },
+    }
+  },
+  typescript: {
+    check: false,
+  },
 }
