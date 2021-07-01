0dc7ae734a64934ab7b9c9f8b756bc201e1111f1
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-07-01 16:27:44 +0200

    Fix nuxt dev with tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    tailwindcss: {},
+    'postcss-nested': {},
+    ...(process.env.NODE_ENV === 'production'
+      ? { tailwindcss: { config: './app/tailwind.config.js' } }
+      : {}),
     autoprefixer: {},
   },
 };
