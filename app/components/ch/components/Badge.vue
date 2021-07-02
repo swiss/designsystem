b0c16f69ba1a6fd24b5cf77fd235c0d1ae626367
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-07-02 14:05:32 +0200

    BDS-61 Add props

---
+++ app/components/ch/components/Badge.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  {{ label }}
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'badge',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'base',
+      validator: (prop) => [
+        'gray',
+        'red',
+        'yellow',
+        'orange',
+        'green',
+        'blue',
+        'indigo',
+        'purple',
+        'pink'
+      ].includes(prop)
+    },
+    size: {
+      type: String,
+      validator: (prop) => [
+        'xs',
+        'sm',
+      ].includes(prop)
+    },
+    remove: {
+      type: Boolean,
+      default: false,
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+  },
+};
+</script>
