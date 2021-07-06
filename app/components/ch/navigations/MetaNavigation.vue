9864b43fbc44096c735454560f4633872f6fa1ff
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-07-06 10:33:02 +0200

    BDS-45 init topbar structure

---
+++ app/components/ch/navigations/MetaNavigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="metanavigation">
+    <button>test</button>
+    <nav aria-labelledby="metanav-label">
+	    <h2 id="metanav-label" class="hidden">Meta Menu</h2>
+    </nav>
+  </div>
+</template>
--- app/components/ch/sections/TopBar.vue
+++ app/components/ch/sections/TopBar.vue
@@ -1,7 +1,43 @@
 <template>
-  <div class="top-bar">
-    <div class="container">
-      Top bar
+  <div
+    class="top-bar"
+    :class="isOpen ? 'top-bar--is-open' : '' "
+  >
+    <div class="top-bar__bar">
+      <div class="container">
+        <button
+          class="top-bar__btn"
+          @click="isOpen = !isOpen"
+        >
+          Alle Schweizer Bundesbehörden
+        </button>
+        <div class="top-bar__meta">
+          <MetaNavigation />
+        </div>
+      </div>
+    </div>
+    <div v-if="isOpen" class="top-bar__content">
+      <div class="container">
+        Top bar content
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import MetaNavigation from '../navigations/MetaNavigation.vue'
+
+export default {
+  name: 'Logo',
+  components: {
+    MetaNavigation
+  },
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  },
+}
+</script>
