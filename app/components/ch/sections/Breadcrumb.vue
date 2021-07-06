7015609e56779e2d5fbcf554cdbb217e78bbb00e
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-07-06 17:47:06 +0200

    BDS-45 minimal breadcrumb

--- app/components/ch/sections/Breadcrumb.vue
+++ app/components/ch/sections/Breadcrumb.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="breadcrumb">
-    <div class="container">
-      Breadcrumb
-    </div>
+  <div class="container container--flex">
+    <nav
+      aria-label="Breadcrumb navigation"
+    >
+      <ol class="breadcrumb">
+        <li><a href="#">Startseite</a></li>
+        <li><a href="#">Diensleistungen</a></li>
+        <li><a href="#">Geodienste</a></li>
+        <li><a href="#">Datenmodellablage</a></li>
+      </ol>
+    </nav>
   </div>
 </template>
