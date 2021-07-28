eafefdf6bd9fd53282b790b73b1d89b02250dc9f
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-07-28 09:49:13 +0200

    BDS-76 Create Pagination story

---
+++ app/components/ch/components/Pagination.vue
@@ -0,0 +1,47 @@
+<template>
+  <button type="button" :class="classes">
+    <span
+      v-if="this.label"
+      class="btn__text"
+    >
+      {{ label }}
+    </span>
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'pagination',
+  props: {
+    type: {
+      type: String,
+      validator: (prop) => [
+        'outline',
+        'outline-negative',
+      ].includes(prop)
+    },
+    size: {
+      type: String,
+      validator: (prop) => [
+        'sm',
+        'base',
+        'lg'
+      ].includes(prop)
+    },
+    label: {
+      type: String,
+      required: true,
+    }
+  },
+
+  computed: {
+    classes () {
+      let base = 'pagination '
+      if (this.type) base += `pagination--${this.type} `
+      if (this.size) base += `pagination--${this.size} `
+      return base
+    },
+  }
+}
+</script>
