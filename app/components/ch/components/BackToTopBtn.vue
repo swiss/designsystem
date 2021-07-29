c74e5dc984b371afa2acb059e5ec9bfa6419b971
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-07-29 18:15:25 +0200

    BDS-92 Add back to top button story

---
+++ app/components/ch/components/BackToTopBtn.vue
@@ -0,0 +1,37 @@
+<template>
+  <button type="button" :class="classes">
+    <SvgIcon
+      icon="ChevronSmallUp"
+      class="back-to-top-btn__icon"
+    />
+  </button>
+</template>
+
+<script>
+import SvgIcon from '../components/SvgIcon.vue';
+
+export default {
+  name: 'BackToTopBtn',
+  components: {
+    SvgIcon
+  },
+  props: {
+    type: {
+      type: String,
+      validator: (prop) => [
+        'default',
+        'outline',
+        'negative'
+      ].includes(prop)
+    },
+  },
+
+  computed: {
+    classes () {
+      let base = 'back-to-top-btn '
+      if (this.type) base += `back-to-top-btn--${this.type} `
+      return base
+    },
+  }
+}
+</script>
