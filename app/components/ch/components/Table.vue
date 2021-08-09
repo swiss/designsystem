4082c9f97a8cc9a21e28145d22fe196f6e4e42ee
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-08-09 08:49:30 +0200

    BDS-48 Add simple table

---
+++ app/components/ch/components/Table.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="table-container">
+    <table class="table">
+      <thead class="table__header">
+        <th class="table__header--entry">Name</th>
+        <th class="table__header--entry">Job</th>
+        <th class="table__header--entry">E-mail</th>
+        <th class="table__header--entry">Role</th>
+      </thead>
+      <tbody>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Jane Cooper
+          </td>
+          <td class="table__row--entry">Regional Paradigm Technician</td>
+          <td class="table__row--entry">jane.cooper@example.com</td>
+          <td class="table__row--entry">Admin</td>
+        </tr>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Cody Fisher
+          </td>
+          <td class="table__row--entry">Product Directives Officer</td>
+          <td class="table__row--entry">cody.fisher@example.com</td>
+          <td class="table__row--entry">Owner</td>
+        </tr>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Esther Howard
+          </td>
+          <td class="table__row--entry">Forward Response Developer</td>
+          <td class="table__row--entry">esther.howard@example.com</td>
+          <td class="table__row--entry">Member</td>
+        </tr>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Jenny Wilson
+          </td>
+          <td class="table__row--entry">Central Security Manager</td>
+          <td class="table__row--entry">jenny.wilson@example.com</td>
+          <td class="table__row--entry">Member</td>
+        </tr>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Kristin Watson
+          </td>
+          <td class="table__row--entry">Lead Implementation Liaison</td>
+          <td class="table__row--entry">kristin.watson@example.com</td>
+          <td class="table__row--entry">Admin</td>
+        </tr>
+        <tr class="table__row">
+          <td class="table__row--entry table__row--entry-highlight">
+            Cameron Williamson
+          </td>
+          <td class="table__row--entry">Internal Applications Engineer</td>
+          <td class="table__row--entry">cameron.williamson@example.com</td>
+          <td class="table__row--entry">Member</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
