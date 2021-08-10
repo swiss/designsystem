a28e749ba0294a68d432c18b18a7e9772c03e377
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-08-10 16:38:29 +0200

    BDS-59 notifications fine tuning

--- app/components/ch/components/Notification.vue
+++ app/components/ch/components/Notification.vue
@@ -1,7 +1,52 @@
 <template>
-  <div>
-    <div class="notification is-information">
-      A new software update is available. See what's new in version 2.0.4.
-    </div>
+<div :class="classes">
+  <SvgIcon
+    v-if="this.icon"
+    :icon="this.icon"
+    class="notification__icon"
+  />
+  <div class="notification__text">
+    {{ text }}
   </div>
+</div>
 </template>
+
+<script>
+import SvgIcon from '../components/SvgIcon.vue';
+
+export default {
+  name: 'notification',
+  components: {
+    SvgIcon
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: undefined,
+      validator: (prop) => [
+        'info',
+        'warning',
+        'error',
+        'success'
+      ].includes(prop)
+    },
+    icon: {
+      type: String,
+      default: undefined
+    },
+  },
+
+  computed: {
+    classes () {
+      let base = 'notification '
+      if (this.type) base += `notification--${this.type} `
+      return base
+    },
+  }
+};
+</script>
+
