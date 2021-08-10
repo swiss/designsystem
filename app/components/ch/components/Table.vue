491acdc3a4bce3be9c493cf989edd98ca87cf956
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-08-10 07:21:19 +0200

    BDS-79 Switch to html elements instead of classes

--- app/components/ch/components/Table.vue
+++ app/components/ch/components/Table.vue
@@ -1,62 +1,48 @@
 <template>
-  <div class="table-container">
-    <table class="table">
-      <thead class="table__header">
-        <th class="table__header--entry">Name</th>
-        <th class="table__header--entry">Job</th>
-        <th class="table__header--entry">E-mail</th>
-        <th class="table__header--entry">Role</th>
-      </thead>
-      <tbody>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Jane Cooper
-          </td>
-          <td class="table__row--entry">Regional Paradigm Technician</td>
-          <td class="table__row--entry">jane.cooper@example.com</td>
-          <td class="table__row--entry">Admin</td>
-        </tr>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Cody Fisher
-          </td>
-          <td class="table__row--entry">Product Directives Officer</td>
-          <td class="table__row--entry">cody.fisher@example.com</td>
-          <td class="table__row--entry">Owner</td>
-        </tr>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Esther Howard
-          </td>
-          <td class="table__row--entry">Forward Response Developer</td>
-          <td class="table__row--entry">esther.howard@example.com</td>
-          <td class="table__row--entry">Member</td>
-        </tr>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Jenny Wilson
-          </td>
-          <td class="table__row--entry">Central Security Manager</td>
-          <td class="table__row--entry">jenny.wilson@example.com</td>
-          <td class="table__row--entry">Member</td>
-        </tr>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Kristin Watson
-          </td>
-          <td class="table__row--entry">Lead Implementation Liaison</td>
-          <td class="table__row--entry">kristin.watson@example.com</td>
-          <td class="table__row--entry">Admin</td>
-        </tr>
-        <tr class="table__row">
-          <td class="table__row--entry table__row--entry-highlight">
-            Cameron Williamson
-          </td>
-          <td class="table__row--entry">Internal Applications Engineer</td>
-          <td class="table__row--entry">cameron.williamson@example.com</td>
-          <td class="table__row--entry">Member</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table>
+    <thead>
+      <th>Name</th>
+      <th>Job</th>
+      <th>E-mail</th>
+      <th>Role</th>
+    </thead>
+    <tbody>
+      <tr>
+        <th scopre="row">Jane Cooper</th>
+        <td>Regional Paradigm Technician</td>
+        <td>jane.cooper@example.com</td>
+        <td>Admin</td>
+      </tr>
+      <tr>
+        <th scope="row">Cody Fisher</th>
+        <td>Product Directives Officer</td>
+        <td>cody.fisher@example.com</td>
+        <td>Owner</td>
+      </tr>
+      <tr>
+        <th scope="row">Esther Howard</th>
+        <td>Forward Response Developer</td>
+        <td>esther.howard@example.com</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <th scope="row">Jenny Wilson</th>
+        <td>Central Security Manager</td>
+        <td>jenny.wilson@example.com</td>
+        <td>Member</td>
+      </tr>
+      <tr>
+        <th scope="row">Kristin Watson</th>
+        <td>Lead Implementation Liaison</td>
+        <td>kristin.watson@example.com</td>
+        <td>Admin</td>
+      </tr>
+      <tr>
+        <th scope="row">Cameron Williamson</th>
+        <td>Internal Applications Engineer</td>
+        <td>cameron.williamson@example.com</td>
+        <td>Member</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
