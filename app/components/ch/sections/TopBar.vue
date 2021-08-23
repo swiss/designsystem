e923b27bb9f3823613ce6172edd1faf2bcceed15
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-08-23 17:13:27 +0200

    BDS-57 Add agencies informations

--- app/components/ch/sections/TopBar.vue
+++ app/components/ch/sections/TopBar.vue
@@ -44,6 +44,123 @@
           </div>
         </div>
 
+        <div class="top-bar__drawer__agencies">
+          <h3>Eine andere Bundesbehörde besuchen</h3>
+
+          <ul class="top-bar__drawer__agencies__information">
+            <li class="top-bar__drawer__agencies__information__entry entry__websites">
+              <div class="entry__website">
+                <h4>Zur Bundesrat Webseite gehen</h4>
+                <p>
+                  Sieben Departementsvorsteherinnen und Departementsvorsteher bilden den Bundesrat.
+                  Entschieden wird gemeinsam. Der Bundeskanzler unterstützt die Regierung.
+                </p>
+                <Btn label="Bundesrat Webseite besuchen" icon="External" icon-pos="right" type="outline-negative" size="sm" />
+              </div>
+              <div class="entry__website">
+                <h4>Zur Bundeskanzlei Webseite gehen</h4>
+                <p>
+                  Die Bundeskanzlei ist die Stabsstelle des Bundesrats und hat die Funktion eines Scharniers zwischen Regierung,
+                  Verwaltung, Parlament und Öffentlichkeit.
+                </p>
+                <Btn label="Bundeskanzlei Webseite besuchen" icon="External" icon-pos="right" type="outline-negative" size="sm" />
+              </div>
+            </li>
+            <li class="top-bar__drawer__agencies__information__entry">
+              <div class="entry__departments">
+                <h4>Alle Departemente</h4>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">EDA</span>
+                    <p>Eidgenössisches Departement für auswärtige Angelegenheiten</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">EDI</span>
+                    <p>Eidgenössisches Departement des Innern</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">EJPD</span>
+                    <p>Eidgenössisches Justiz- und Polizeidepartement</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">VBS</span>
+                    <p>Eidgenössisches Departement für Verteidigung, Bevölkerungsschutz und Sport</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">EFD</span>
+                    <p>Eidgenössisches Finanzdepartement</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">WBF</span>
+                    <p>Eidgenössisches Departement für Wirtschaft, Bildung und Forschung</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+                <div class="entry__department">
+                  <div class="entry__department__info">
+                    <span class="entry__department__acronym">UVEK</span>
+                    <p>Eidgenössisches Departement für Umwelt, Verkehr, Energie und Kommunikation</p>
+                  </div>
+                  <a href="javascript:void(0)">
+                    <SvgIcon
+                      icon="External"
+                      class="entry__department__icon"
+                    />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li class="top-bar__drawer__agencies__information__entry">
+              <h4>Alle Ämter</h4>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   </div>
@@ -51,13 +168,15 @@
 
 <script>
 import TopBarNavigation from '../navigations/TopBarNavigation.vue'
-import SvgIcon from '../components/SvgIcon.vue';
+import SvgIcon from '../components/SvgIcon.vue'
+import Btn from "~/components/ch/components/Btn";
 
 export default {
   name: 'TopBar',
   components: {
     TopBarNavigation,
-    SvgIcon
+    SvgIcon,
+    Btn
   },
   props: {
     isOpen: {
