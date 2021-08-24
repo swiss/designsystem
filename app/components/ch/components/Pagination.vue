107c20d6f9a42d94fa75c82757bfc61832d21b04
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-08-24 16:05:31 +0200

    BDS-92 Possibility to add icons on pagination + update arial-label

--- app/components/ch/components/Pagination.vue
+++ app/components/ch/components/Pagination.vue
@@ -1,13 +1,28 @@
 <template>
-  <button type="button" :class="classes" aria-label="pagination">
-    {{ label }}
+  <button type="button" :class="classes" :aria-label="ariaLabel">
+    <SvgIcon
+      v-if="this.icon !== 'none'"
+      :icon="this.icon"
+      class="pagination__icon"
+    />
+    <span
+      v-if="this.label"
+      class="pagination__text"
+    >
+      {{ label }}
+    </span>
   </button>
 </template>
 
 <script>
 
+import SvgIcon from "~/components/ch/components/SvgIcon";
+
 export default {
   name: 'pagination',
+  components: {
+    SvgIcon
+  },
   props: {
     type: {
       type: String,
@@ -24,6 +39,10 @@ export default {
         'lg'
       ].includes(prop)
     },
+    icon: {
+      type: String,
+      default: 'none'
+    },
     label: {
       type: String,
       required: true,
@@ -35,8 +54,18 @@ export default {
       let base = 'pagination '
       if (this.type) base += `pagination--${this.type} `
       if (this.size) base += `pagination--${this.size} `
+      if (this.icon !== 'none') base += `pagination--icon-only `
       return base
     },
+    ariaLabel () {
+      let ariaLabel = ''
+      if (this.icon === 'ChevronLineLeft') ariaLabel = 'First page'
+      if (this.icon === 'ChevronLeft') ariaLabel = 'Previous page'
+      if (this.icon === 'ChevronLineRight') ariaLabel = 'Last page'
+      if (this.icon === 'ChevronRight') ariaLabel = 'Next page'
+      if (this.icon === 'none') ariaLabel = `Page ${this.label}`
+      return ariaLabel
+    }
   }
 }
 </script>
