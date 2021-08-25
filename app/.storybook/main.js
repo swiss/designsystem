e03ff026329b3dcf7677f86d0850c2a0156c0749
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-08-25 08:15:06 +0200

    BDS-97 Fix storybook tailwind setup

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,13 +1,27 @@
-module.exports = {
-  "stories": [
-    "../components/stories/**/*.stories.mdx",
-    "../components/stories/**/*.stories.@(js|jsx|ts|tsx)"
+const { nuxifyStorybook } = require('../.nuxt-storybook/storybook/main.js')
+const path = require('path')
+
+module.exports = nuxifyStorybook({
+  stories: [
+    '../components/stories/**/*.stories.mdx',
+    '../components/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@whitespace/storybook-addon-html",
-    "storybook-addon-designs"
-  ]
-}
+  addons: ['@storybook/addon-links', 'storybook-addon-designs'],
+  webpackFinal(config, options) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+    return config
+  },
+})
