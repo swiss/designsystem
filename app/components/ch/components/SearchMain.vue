b79e1911fac9595ced6e2939c1f3eabf5a0cedcf
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-08-30 22:36:42 +0200

    BDS-46 add main search component

---
+++ app/components/ch/components/SearchMain.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search search--main">
+    <div class="search__group">
+      <Input type="text" id="text-id-0" label="Amt suchen" placeholder="Amt suchen" />
+      <!--Btn label="Amt Suchen" icon="Search" icon-pos="only" /-->
+      <button
+        class="search-opener"
+        title="Toggle search"
+      >
+        <span class="search-opener__title">Search</span>
+        <SvgIcon
+          icon="Search" size="full"
+          class="search-opener__icon"
+        />
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import SvgIcon from './SvgIcon.vue';
+import Input from './Input.vue';
+import Btn from './Btn.vue';
+
+export default {
+  components: {
+    SvgIcon,
+    Input,
+    Btn
+  },
+	name: 'SearchMain',
+	props: {
+		isOpen: {
+			type: Boolean,
+			default: false,
+		}
+	}
+}
+</script>
