3946fe7f486428c2848775d745e623740998ac1c
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-08-31 10:37:48 +0200

    BDS-16 Add class container

--- app/components/ch/sections/FooterNavigation.vue
+++ app/components/ch/sections/FooterNavigation.vue
@@ -1,10 +1,14 @@
 <template>
-  <ul class="footer-navigation">
-    <li><a href="javascript:void(0)" class="footer__link">Rechtliches</a></li>
-    <li><a href="javascript:void(0)" class="footer__link">Datenschutz</a></li>
-    <li><a href="javascript:void(0)" class="footer__link">Impressum</a></li>
-    <li>
-      <a href="javascript:void(0)" class="footer__link">Barrierefreiheit</a>
-    </li>
-  </ul>
+  <div class="bg--secondary-700">
+    <div class="container">
+      <ul class="footer-navigation">
+        <li><a href="javascript:void(0)" class="footer__link">Rechtliches</a></li>
+        <li><a href="javascript:void(0)" class="footer__link">Datenschutz</a></li>
+        <li><a href="javascript:void(0)" class="footer__link">Impressum</a></li>
+        <li>
+          <a href="javascript:void(0)" class="footer__link">Barrierefreiheit</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
