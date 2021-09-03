d8b6dd2c016157626d39c91c0b68b1be0c3eeb80
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-09-03 10:01:37 +0200

    BDS-67 Create HeroDetailComplex

---
+++ app/components/ch/sections/HeroDetailComplex.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pt-8 pb-32">
+    <div class="container container--md mx-auto">
+      <Hero type="wide">
+        <template v-slot:title>
+          Datenmodellablage: Model Repository für Geobasisdaten des Bundesrechts
+        </template>
+      </Hero>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hero from "~/components/ch/components/Hero";
+
+export default {
+  name: 'HeroDetailComplex',
+  components: {
+    Hero,
+  },
+};
+</script>
---
+++ app/pages/detailPageComplex.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <AlterBodyClasses :isMobileMenuOpen="getMobileMenuIsOpen()" />
+    <header id="main-header">
+      <a href="#main-content" class="skip-to-content">Skip to main content</a>
+      <TopBar :isOpen="false" />
+      <TopHeader />
+      <DesktopMenu />
+      <MobileMenu />
+      <Breadcrumb />
+    </header>
+    <main id="main-content">
+      <HeroDetailComplex />
+    </main>
+    <footer class="footer" id="main-footer">
+      <footer-information />
+      <footer-navigation />
+    </footer>
+  </div>
+</template>
+
+<script>
+import AlterBodyClasses from '../components/ch/objects/AlterBodyClasses.vue'
+import TopBar from '../components/ch/sections/TopBar.vue'
+import TopHeader from '../components/ch/sections/TopHeader.vue'
+import Breadcrumb from '../components/ch/sections/Breadcrumb.vue'
+import DesktopMenu from '../components/ch/sections/DesktopMenu.vue'
+import MobileMenu from '../components/ch/sections/MobileMenu.vue'
+import FooterInformation from '../components/ch/sections/FooterInformation.vue'
+import FooterNavigation from '../components/ch/sections/FooterNavigation.vue'
+import HeroDetailComplex from "~/components/ch/sections/HeroDetailComplex";
+
+export default {
+  name: 'detailPageSimple',
+  components: {
+    AlterBodyClasses,
+    TopBar,
+    TopHeader,
+    Breadcrumb,
+    DesktopMenu,
+    MobileMenu,
+    FooterInformation,
+    FooterNavigation,
+    HeroDetailComplex
+  },
+  methods: {
+    getMobileMenuIsOpen() {
+      return this.$store.getters['layout/getMobileMenuIsOpen']
+    },
+  },
+}
+</script>
