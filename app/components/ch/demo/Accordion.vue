41afbfc7ea30ec9d4d88229b5dee19eb068d5b03
Sylvain Aerni <sylvain.aerni@gmail.com>
2021-09-13 09:10:26 +0200

    BDS-58 init Accordion example

---
+++ app/components/ch/demo/Accordion.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul id="accordion">
+  <li>
+    <button aria-controls="content-1" aria-expanded="false" id="accordion-control-1">FAQ 1</button>
+    <div class="acc-item-content" aria-hidden="true" id="content-1">
+      <p>Answer 1!</p>
+    </div>
+  </li>
+  <li>
+    <button aria-controls="content-2" aria-expanded="false" id="accordion-control-2">FAQ 2</button>
+    <div class="acc-item-content" aria-hidden="true" id="content-2">
+      <p>Answer 2</p>
+    </div>
+  </li>
+  <li>
+    <button aria-controls="content-3" aria-expanded="false" id="accordion-control-3">FAQ 3</button>
+    <div class="acc-item-content" aria-hidden="true" id="content-3">
+      <p>Answer 3</p>
+    </div>
+  </li>
+  <li>
+    <button aria-controls="content-4" aria-expanded="false" id="accordion-control-4">FAQ 4 </button>
+    <div class="acc-item-content" aria-hidden="true" id="content-4">
+      <p>Answer 4</p>
+    </div>
+  </li>
+  <li>
+    <button aria-controls="content-5" aria-expanded="false" id="accordion-control-5">FAQ 5</button>
+    <div class="acc-item-content" aria-hidden="true" id="content-5">
+      <p>Answer 5</p>
+    </div>
+  </li>
+</ul>
+</template>
