06b0c6cd9bb83e99e2f1ac4ddac1d172b3e5ecb2
Sylvain Aerni <sylvain.aerni@liip.ch>
2022-03-11 15:35:15 +0100

    BDS#147 Carousel for pictures

---
+++ app/components/ch/demo/CarouselExample.vue
@@ -0,0 +1,133 @@
+<template>
+  <div 
+    :class="computedClasses"
+  >
+    <swiper
+      :loop="loop"
+      :breakpoints="breakpoints"
+      :speed="500"
+      :autoHeight="true"
+      :keyboard="{
+        enabled: true,
+        onlyInViewport: false,
+      }"
+      :navigation="{
+        nextEl: `#carousel-next-${id}`,
+        prevEl: `#carousel-prev-${id}`
+      }"
+      :simulateTouch="true"
+      :slideToClickedSlide="false"
+      :pagination="{
+          type: paginationType,
+          el: `#carousel-pagination-${id}`,
+          clickable: true,
+          bulletElement: 'button',
+          bulletClass: 'carousel__bullet',
+          bulletActiveClass: 'carousel__bullet--active'
+      }"
+      :slides-per-view="1"
+      :space-between="16"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide
+        v-for="(slide, index) in slides"
+        :key="`slide-${index}`"
+      >
+        <figure>
+          <picture>
+            <source
+              :srcset="slide.source.srcset"
+              :height="slide.source.height"
+              :width="slide.source.width"
+              :media="slide.source.media"
+            />
+            <img 
+              :src="slide.image.src" 
+              :alt="slide.image.alt" 
+              :height="slide.image.height"
+              :width="slide.image.width"
+            />
+          </picture>
+          <figcaption v-html="slide.caption" />
+        </figure>
+      </swiper-slide>
+    </swiper>
+    <div class="carousel__fonctions">
+      <div class="carousel__pagination" :id="`carousel-pagination-${id}`"></div>
+      <button
+        class="carousel__prev" 
+        :id="`carousel-prev-${id}`"
+      >
+        <div class="sr-only">Previous image</div>
+        <SvgIcon
+          icon="ChevronLeft"
+          role="presentation" 
+          aria-hidden="true"
+        />
+      </button>
+      <button 
+        class="carousel__next" 
+        :id="`carousel-next-${id}`"
+      >
+        <div class="sr-only">Next image</div>
+        <SvgIcon
+          icon="ChevronRight"
+          role="presentation" 
+          aria-hidden="true"
+        />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Navigation, Pagination, A11y } from 'swiper'
+import { SwiperCore, Swiper, SwiperSlide } from 'swiper-vue2'
+
+import SvgIcon from '../components/SvgIcon.vue'
+
+SwiperCore.use([Navigation, Pagination])
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+    SvgIcon
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 1
+    },
+    loop: {
+      type: Boolean,
+      default: false
+    },
+    breakpoints: {
+      type: Object
+    },
+    paginationType: {
+      type: String,
+      validator: (prop) => [
+        'bullets',
+        'fraction',
+        'progressbar'
+      ].includes(prop),
+      default: 'bullets'
+    },
+    slides: {
+      type: Array,
+      required: true
+    }
+  },
+
+  computed: {
+    computedClasses () {
+      let base = 'carousel '
+      if (this.paginationType) base += `carousel--${this.paginationType} `
+      return base
+    },
+  }
+}
+</script>
