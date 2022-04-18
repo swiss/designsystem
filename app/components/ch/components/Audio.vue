68bc1faf463110b308e7e5b8ba4fbce0ef52f306
Sylvain Aerni <sylvain.aerni@liip.ch>
2022-04-18 17:18:32 +0200

    BDS#82 init audio

---
+++ app/components/ch/components/Audio.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    test audio
+    <audio 
+      id="audio_with_controls" 
+      controls
+      :src="url"
+      :type="type" 
+    >
+      Ihr Browser kann dieses Tondokument nicht wiedergeben.<br>
+      Es enthält eine Aufführung der Europahymne. 
+      Sie können es unter
+      <a href="/local/Europahymne.mp3">diesem Link</a>
+      abrufen.
+</audio>
+  </div>
+</template>
+
+<script>
+import SvgIcon from '../components/SvgIcon.vue'
+import MetaInfo from './MetaInfo.vue'
+export default {
+  name: 'DownloadItem',
+  components: {
+    SvgIcon,
+    MetaInfo,
+  },
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "audio/mp3"
+    }
+  },
+}
+</script>
