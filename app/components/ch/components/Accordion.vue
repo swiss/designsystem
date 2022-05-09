30749e8a3bed5479dcbce47b44e4dff807ca347d
Sylvain Aerni <sylvain.aerni@liip.ch>
2022-05-09 17:51:03 +0200

    BDS#197 refactor Accordion, aloowing components as content

--- app/components/ch/components/Accordion.vue
+++ app/components/ch/components/Accordion.vue
@@ -4,49 +4,17 @@
     class="accordion "
     :class="spaced ? 'accordion--spaced' : ''"
   >
-  <li
-    v-for="(item, key) in content"
-    :key="`${key}`"
-    class="accordion__item"
-  >
-    <button
-      class="accordion__button"
-      aria-expanded="false"
-      :aria-controls="`content-${id}-${key}`"
-      :id="`accordion-control-${id}-${key}`"
-    >
-      <h3 class="accordion__title">
-        {{ item.title }}
-      </h3>
-      <SvgIcon
-        icon="ChevronDown"
-        size="xl"
-        class="accordion__arrow"
-      />
-    </button>
-    <div
-      class="accordion__drawer"
-      aria-hidden="true"
-      :id="`content-${id}-${key}`"
-    >
-      <div
-        class="accordion__content"
-        v-html="item.content"
-      />
-    </div>
-  </li>
+    <slot />
 </ul>
 </template>
 
 <script>
 import Accordion from '../../../scripts/Accordion.js'
-import SvgIcon from '../components/SvgIcon.vue'
 
 export default {
   name: 'Accordion',
   components: {
-    Accordion,
-    SvgIcon
+    Accordion
   },
   props: {
     id: {
@@ -56,10 +24,6 @@ export default {
     spaced: {
       type: Boolean,
       default: false
-    },
-    content: {
-      type: Array,
-      required: true
     }
   },
   
---
+++ app/components/ch/components/AccordionItem.vue
@@ -0,0 +1,67 @@
+<template>
+  <li
+    class="accordion__item"
+  >
+    <button
+      class="accordion__button"
+      aria-expanded="false"
+      :aria-controls="`content-${id}`"
+      :id="`accordion-control-${id}`"
+    >
+      <component
+        :is="headinglevel"
+        class="accordion__title"
+      >
+        {{ title }}
+      </component>
+      <SvgIcon
+        icon="ChevronDown"
+        size="xl"
+        class="accordion__arrow"
+      />
+    </button>
+    <div
+      class="accordion__drawer"
+      aria-hidden="true"
+      :id="`content-${id}`"
+    >
+      <div class="accordion__content">
+        <slot />
+      </div>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+import SvgIcon from '../components/SvgIcon.vue'
+import MetaInfo from './MetaInfo.vue'
+export default {
+  name: 'AccordionItem',
+  components: {
+    SvgIcon,
+    MetaInfo,
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    headinglevel: {
+      type: String,
+      default: 'h3',
+      validator: (prop) => [
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'div'
+      ].includes(prop)
+    }
+  },
+}
+</script>
