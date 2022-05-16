f079e54dae488194bdadd05a01d355e093497043
David Darx <david.darx@liip.ch>
2022-05-16 12:51:23 +0200

    BDS#124 add basic componenet

---
+++ app/components/ch/components/Newsletter.vue
@@ -0,0 +1,35 @@
+<template>
+<div :class="classes">
+  I am a newsletter module
+</div>
+</template>
+
+<script>
+import SvgIcon from './Notification.vue';
+
+export default {
+  name: 'newsletter',
+  components: {
+    Notification
+  },
+  props: {
+    state: {
+      state: String,
+      default: undefined,
+      validator: (prop) => [
+        'default',
+        'sent'
+      ].includes(prop)
+    }
+  },
+
+  computed: {
+    classes () {
+      let base = 'newsletter '
+      if (this.state) base += `notification--${this.state} `
+      return base
+    },
+  }
+};
+</script>
+
