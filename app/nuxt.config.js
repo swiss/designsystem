2674f772f92a91f069c86f7f7806923be8526f0e
Sylvain Aerni <sylvain.aerni@liip.ch>
2022-06-20 13:41:46 +0200

    BDS#251 fix notification banner on Android

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
