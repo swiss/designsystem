7c9ce8b23d94a4d1a7c8d106985b8d7265964632
Sylvain Aerni <sylvain.aerni@liip.ch>
2022-12-12 10:46:34 +0100

    BDS#309 add aria label in footer nav

--- app/components/ch/sections/FooterNavigation.vue
+++ app/components/ch/sections/FooterNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg--secondary-700">
-    <nav class="container">
+    <nav class="container" aria-label="Footer">
       <ul class="footer-navigation">
         <li><a href="javascript:void(0)" class="footer__link">Rechtliches</a></li>
         <li><a href="javascript:void(0)" class="footer__link">Datenschutz</a></li>
