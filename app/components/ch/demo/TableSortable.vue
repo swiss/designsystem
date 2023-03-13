c88dc631cfbb81178dce5ca3f2653e93bd59ad78
Sylvain Aerni <sylvain.aerni@liip.ch>
2023-03-13 15:18:47 +0100

    BDS#340 improve tables and better documentation

--- app/components/ch/demo/TableSortable.vue
+++ app/components/ch/demo/TableSortable.vue
@@ -1,25 +1,32 @@
 <template>
   <table :class="computedClasses" :id="`table-sortable-${id}`">
-    <caption >
-      Short description of the table content for better accessibility
+    <caption>
+      {{ caption }}
       <span class="sr-only">, column headers with buttons are sortable.</span>
     </caption>
     <thead>
       <tr>
         <th scope="col">
-          <button>
+          <button class="table__sorter">
             Name
             <span aria-hidden="true"></span>
           </button>
         </th>
-        <th aria-sort="ascending">
-          <button>
+        <th scope="col" aria-sort="ascending">
+          <button class="table__sorter">
             Job
             <span aria-hidden="true"></span>
           </button>
         </th>
-        <th scope="col">E-mail</th>
-        <th scope="col">Role</th>
+        <th scope="col">
+          E-mail
+        </th>
+        <th scope="col" class="text-right">
+          <button class="table__sorter">
+            ID
+            <span aria-hidden="true"></span>
+          </button>
+        </th>
       </tr>
     </thead>
     <tbody>
@@ -27,37 +34,37 @@
         <th scope="row">Jane Cooper</th>
         <td>Regional Paradigm Technician</td>
         <td>jane.cooper@example.com</td>
-        <td>Admin</td>
+        <td class="text-right">200</td>
       </tr>
       <tr>
         <th scope="row">Cody Fisher</th>
         <td>Product Directives Officer</td>
         <td>cody.fisher@example.com</td>
-        <td>Owner</td>
+        <td class="text-right">100</td>
       </tr>
       <tr>
         <th scope="row">Esther Howard</th>
         <td>Forward Response Developer</td>
         <td>esther.howard@example.com</td>
-        <td>Member</td>
+        <td class="text-right">400</td>
       </tr>
       <tr>
         <th scope="row">Jenny Wilson</th>
         <td>Central Security Manager</td>
         <td>jenny.wilson@example.com</td>
-        <td>Member</td>
+        <td class="text-right">600</td>
       </tr>
       <tr>
         <th scope="row">Kristin Watson</th>
         <td>Lead Implementation Liaison</td>
         <td>kristin.watson@example.com</td>
-        <td>Admin</td>
+        <td class="text-right">300</td>
       </tr>
       <tr>
         <th scope="row">Cameron Williamson</th>
         <td>Internal Applications Engineer</td>
         <td>cameron.williamson@example.com</td>
-        <td>Member</td>
+        <td class="text-right">500</td>
       </tr>
     </tbody>
   </table>
@@ -76,6 +83,14 @@ export default {
         'default',
       ].includes(prop)
     },
+    caption: {
+      type: String,
+      default: ''
+    },
+    displayCaption: {
+      type: Boolean,
+      default: true
+    },
     zebra: {
       type: Boolean,
       default: false
@@ -90,6 +105,7 @@ export default {
     computedClasses () {
       let base = 'table table--sortable '
       if (this.variant) base += `table--${this.variant} `
+      if (this.displayCaption) base += `table--caption `
       if (this.zebra) base += `table--zebra `
       return base
     }
@@ -100,3 +116,5 @@ export default {
   },
 }
 </script>
+
+
