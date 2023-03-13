f1a991bf97e693eb21c94fb77891c5cc92348a0d
Sylvain Aerni <sylvain.aerni@liip.ch>
2023-03-13 16:06:45 +0100

    BDS#340 column right and tfoot

--- app/components/ch/demo/Table.vue
+++ app/components/ch/demo/Table.vue
@@ -8,7 +8,7 @@
         <th scope="col">Name</th>
         <th scope="col">Job</th>
         <th scope="col">E-mail</th>
-        <th scope="col">Role</th>
+        <th scope="col" class="text-right">Points</th>
       </tr>
     </thead>
     <tbody>
@@ -16,39 +16,47 @@
         <th scope="row">Jane Cooper</th>
         <td>Regional Paradigm Technician</td>
         <td>jane.cooper@example.com</td>
-        <td>Admin</td>
+        <td class="text-right">20</td>
       </tr>
       <tr>
         <th scope="row">Cody Fisher</th>
         <td>Product Directives Officer</td>
         <td>cody.fisher@example.com</td>
-        <td>Owner</td>
+        <td class="text-right">30</td>
       </tr>
       <tr>
         <th scope="row">Esther Howard</th>
         <td>Forward Response Developer</td>
         <td>esther.howard@example.com</td>
-        <td>Member</td>
+        <td class="text-right">40</td>
       </tr>
       <tr>
         <th scope="row">Jenny Wilson</th>
         <td>Central Security Manager</td>
         <td>jenny.wilson@example.com</td>
-        <td>Member</td>
+        <td class="text-right">50</td>
       </tr>
       <tr>
         <th scope="row">Kristin Watson</th>
         <td>Lead Implementation Liaison</td>
         <td>kristin.watson@example.com</td>
-        <td>Admin</td>
+        <td class="text-right">60</td>
       </tr>
       <tr>
         <th scope="row">Cameron Williamson</th>
         <td>Internal Applications Engineer</td>
         <td>cameron.williamson@example.com</td>
-        <td>Member</td>
+        <td class="text-right">70</td>
       </tr>
     </tbody>
+    <tfoot>
+      <tr>
+        <th scope="row">Total</th>
+        <td colspan="3" class="text-right">
+          270
+        </td>
+      </tr>
+    </tfoot>
   </table>
 </template>
 
