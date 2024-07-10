8f26d0fe2a0dec09f61f9e0d618629d53e78dbb4
taazust5 <stefan.zurkinden@swisscom.com>
2024-07-10 13:02:00 +0200

    feat(mail-template): wip sales mail template

---
+++ app/pages/mailTemplatePreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="mb-3">
+      <p>Order confirmation mail templates</p>
+    </div>
+    <div>
+      <a class="link" href="templates/sales-mail-template.html"
+        >Sales mail template</a
+      >
+    </div>
+    <div class="mt-3">
+      <a class="link mt-8" href="templates/customer-mail-template.html"
+        >Customer mail template</a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MailTemplatePreview',
+}
+</script>
