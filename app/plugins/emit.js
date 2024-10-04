0ebd25402d7f04219c13128e5eabb97cd6deced4
Florian Dendorfer <florian.dendorfer@jls.ch>
2024-10-04 10:43:13 +0200

    Add some todos for later

--- app/plugins/emit.js
+++ app/plugins/emit.js
@@ -3,11 +3,9 @@ const emitter = mitt()
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide('emit', (...args) => {
-    // console.log('plugin emit called', args)
     emitter.emit(...args)
   })
   nuxtApp.provide('on', (...args) => {
-    // console.log('plugin emit on called', args)
     emitter.on(...args)
   })
 })
