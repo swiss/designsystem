9b2d3dd1aa87626143ceccc1195d3b9db8b94a82
gebelj <jonas.gebel@jls.ch>
2024-10-04 17:13:07 +0200

    Fix Print Story

--- app/components/stories/components/Print.stories.js
+++ app/components/stories/components/Print.stories.js
@@ -1,18 +1,13 @@
 import Print from '../../ch/components/Print.vue'
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { Print },
-  template: '<Print />',
-})
-
 export default {
   title: 'Components/Print',
   component: Print,
 }
 
 export const Example = {
-  render: Template.bind({}),
-  name: 'Example',
-  args: {},
+  render: () => ({
+    components: { Print },
+  template: '<Print />',
+  }),
 }
