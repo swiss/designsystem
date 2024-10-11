deaa2d9988bf04c514d99488bb4903d72fffdb83
Florian Dendorfer <florian.dendorfer@jls.ch>
2024-10-11 17:23:17 +0200

    Fix Detail Page Event stories

--- app/components/stories/pages/DetailEvent.stories.js
+++ app/components/stories/pages/DetailEvent.stories.js
@@ -1,30 +1,11 @@
 import detailEvent from '../../../pages/detailEvent.vue'
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { detailEvent },
-  template: '<detailEvent />',
-})
-
 export default {
   title: 'Pages/Detail Page Event',
   component: detailEvent,
-
-  argTypes: {
-    isOpen: {
-      name: 'Is open',
-
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
 }
 
 export const Example = {
-  render: Template.bind({}),
-  name: 'Example',
-
   args: {
     isOpen: false,
   },
