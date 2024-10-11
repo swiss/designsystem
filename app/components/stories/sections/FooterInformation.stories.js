8e75e2ba81f8148bf396febb951ca769c2e9f5e6
gebelj <jonas.gebel@jls.ch>
2024-10-11 15:13:23 +0200

    Fix footer folder stories

--- app/components/stories/sections/FooterInformation.stories.js
+++ app/components/stories/sections/FooterInformation.stories.js
@@ -1,17 +1,8 @@
 import FooterInformation from '../../ch/sections/FooterInformation.vue'
 
-const Template = () => ({
-  props: [],
-  components: { FooterInformation },
-  template: '<FooterInformation />',
-})
-
 export default {
   title: 'Sections/Footer/FooterInformation',
   component: FooterInformation,
 }
 
-export const Example = {
-  render: Template.bind({}),
-  name: 'Example',
-}
+export const Example = {}
