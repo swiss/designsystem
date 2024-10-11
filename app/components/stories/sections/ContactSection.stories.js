b04c8f707e2a60d0500989268567689f2d272985
gebelj <jonas.gebel@jls.ch>
2024-10-11 16:58:26 +0200

    Fix ContactSection story

--- app/components/stories/sections/ContactSection.stories.js
+++ app/components/stories/sections/ContactSection.stories.js
@@ -1,17 +1,8 @@
 import ContactSection from '../../ch/sections/ContactSection.vue'
 
-const Template = () => ({
-  props: [],
-  components: { ContactSection },
-  template: '<ContactSection />',
-})
-
 export default {
   title: 'Sections/Content/Contact',
   component: ContactSection,
 }
 
-export const Example = {
-  render: Template.bind({}),
-  name: 'Example',
-}
+export const Example = {}
