fd2659cf62ed98b75a32e07ba4bf12c93fe81f15
gebelj <jonas.gebel@jls.ch>
2024-10-11 14:04:21 +0200

    Fix input story

--- app/components/stories/components/Input.stories.js
+++ app/components/stories/components/Input.stories.js
@@ -1,22 +1,19 @@
 import Input from '../../ch/components/Input.vue'
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { Input },
-  template:
-    '<Input :type="type" :required="required" :variant="variant" :size="size" :label="label" :hideLabel="hideLabel" :placeholder="placeholder" :value="value" :id="id" :min="min" :max="max" :step="step" :message="message" :messageType="messageType" />',
-})
-
 export default {
   title: 'Components/Form/Input',
   component: Input,
 
   argTypes: {
     type: {
-      control: {
-        type: 'select',
-
-        options: [
+      table: {
+        type: {
+          summary: [
+            'string: color, date, datetime-local, email, file, month, number, password, range, search, tel, text, time, url, week, submit',
+          ]
+        },
+      },
+      options: [
           'color',
           'date',
           'datetime-local',
@@ -34,52 +31,71 @@ export default {
           'week',
           'submit',
         ],
+      control: {
+        type: 'select',
       },
     },
-
     required: {
       control: {
         type: 'boolean',
       },
     },
-
     size: {
+      table: {
+        type: {
+          summary: [
+            'string: sm, base, lg',
+          ]
+        },
+      },
+      options: ['sm', 'base', 'lg'],
       control: {
         type: 'select',
-        options: ['sm', 'base', 'lg'],
       },
     },
-
     variant: {
+      table: {
+        type: {
+          summary: [
+            'string: outline, negative',
+          ]
+        },
+      },
+      options: ['outline', 'negative'],
       control: {
         type: 'select',
-        options: ['outline', 'negative'],
       },
     },
-
     label: {},
-
     hideLabel: {
       control: {
         type: 'boolean',
       },
     },
-
     message: {},
-
     messageType: {
+      table: {
+        type: {
+          summary: [
+            'string: error, warning, success, info',
+          ]
+        },
+      },
+      options: ['error', 'warning', 'success', 'info'],
       control: {
         type: 'select',
-        options: ['error', 'warning', 'success', 'info'],
       },
     },
   },
 }
 
 export const Example = {
-  render: Template.bind({}),
-  name: 'Example',
-
+  render: (args) => ({
+    components: { Input },
+    setup: () => ({ args }),
+    template:
+    '<Input v-bind="args" />',
+  }),
   args: {
     type: 'outline',
     size: 'base',
@@ -94,4 +110,61 @@ export const Example = {
     message: undefined,
     messageType: 'error',
   },
+}
+
+export const NegativeInput = {
+  render: () => ({
+    template:
+    '<input class="input--negative input--base" value="Input text" />',
+  }),
+}
+
+export const DisabledInput = {
+  render: () => ({
+    template:`
+      <div style="display: flex; flex-direction: row; gap: 10pt; width: 90%" >
+        <input class="input--outline input--base" disabled value="Input text" />
+        <input class="input--negative input--base" disabled value="Input text" />
+      </div>
+    `,
+  }),
+}
+
+export const SizesInput = {
+  render: () => ({
+    template:`
+      <div style="display: flex; flex-direction: row; gap: 10pt; width: 90%" >
+        <input class="input--outline input--sm" value="input--sm" />
+        <input class="input--outline input--base" value="input--base" />
+        <input class="input--outline input--lg" value="input--lg" />
+      </div>
+    `,
+  }),
+}
+
+export const RequiredInput = {
+  render: () => ({
+    template:`
+      <div class="form__group__input">
+        <label for="my-id" class="text--base text--asterisk">
+          Name <span class="sr-only">required</span>
+        </label>
+        <input type="text" id="my-id" name="my-id" placeholder="Placeholder" class="input--outline input--base" required />
+      </div>
+    `,
+  }),
+}
+
+export const MessagesInput = {
+  render: () => ({
+    template:`
+      <div class="form__group__input">
+        <label for="my-id" class="text--base text--asterisk">
+          Name <span class="sr-only">required</span>
+        </label>
+        <input type="text" id="my-id" name="my-id" placeholder="Placeholder" class="input--outline input--base input--error" required />
+        <div class="badge badge--sm badge--error">This field is required</div>
+      </div>
+    `,
+  }),
 }
