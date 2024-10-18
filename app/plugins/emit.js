fe8ed624b01b68755e1e535640c0e03858c0e636
gebelj <jonas.gebel@jls.ch>
2024-10-18 16:18:20 +0200

    Revert comments and todo for merge

--- app/plugins/emit.js
+++ app/plugins/emit.js
@@ -3,9 +3,11 @@ const emitter = mitt()
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide('emit', (...args) => {
+    // console.log('plugin emit called', args)
     emitter.emit(...args)
   })
   nuxtApp.provide('on', (...args) => {
+    // console.log('plugin emit on called', args)
     emitter.on(...args)
   })
 })
