a7a7aa86713986afa180cbdcb63a292b65a9ffb3
Florian Dendorfer <flo.dendorfer@gmail.com>
2024-10-29 10:35:10 +0100

    Update main.yml (#120)

--- app/components/stories/pages/OverviewSignLanguage.stories.js
+++ app/components/stories/pages/OverviewSignLanguage.stories.js
@@ -3,20 +3,15 @@ import OverviewSignLanguage from '../../../pages/overviewSignLanguage.vue'
 export default {
   title: 'Pages/Easy & Sign Language/Overview Sign Language',
   component: OverviewSignLanguage,
-  render: (args, { viewMode }) => {
-    if (viewMode === 'docs') {
-      return {
-        components: { OverviewSignLanguage },
-        template: '<OverviewSignLanguage />',
-      }
-    } else {
-      return {
-        components: { OverviewSignLanguage },
-        template:
-          '<OverviewSignLanguage useStickyNavigation="useStickyNavigation" />',
-      }
-    }
-  },
+  render: (args, { viewMode }) => ({
+    setup: () => ({ args, viewMode }),
+    components: { OverviewSignLanguage },
+    template: `
+      <OverviewSignLanguage 
+        v-bind="args"
+        :useStickyNavigation="viewMode === 'docs' ? false : args.useStickyNavigation"
+      />`,
+  }),
 }
 
 export const Example = {
