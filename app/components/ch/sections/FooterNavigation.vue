3c0248ee566d81971b64086df21ff9bea2100ef6
gebelj <jonas.gebel@jls.ch>
2024-12-06 11:40:31 +0100

    Add darkmode select and functionality (only for topHeader atm)

--- app/components/ch/sections/FooterNavigation.vue
+++ app/components/ch/sections/FooterNavigation.vue
@@ -3,12 +3,44 @@
     <nav class="container" aria-label="Footer">
       <ul class="footer-navigation">
         <li>
-          <a href="javascript:void(0)" class="footer__link">Rechtliches</a>
+          <ul class="footer-navigation-left">
+            <li>
+              <a href="javascript:void(0)" class="footer__link">Rechtliches</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" class="footer__link">Datenschutz</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" class="footer__link">Impressum</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" class="footer__link">Barrierefreiheit</a>
+            </li>
+          </ul>
         </li>
-        <li>
-          <a href="javascript:void(0)" class="footer__link">Datenschutz</a>
+        <li class="footer-navigation-right">
+          <Select
+            class=""
+            :required="false"
+            variant="negative"
+            size="sm"
+            label="Darstellung"
+            :on-select="handleSelect"
+          >
+            <option selected value="auto">Automatisch</option>
+            <option value="dark">Darkmode</option>
+            <option value="light">Lightmode</option>
+          </Select>
         </li>
       </ul>
     </nav>
   </div>
 </template>
+
+<script setup lang="ts">
+import Select from '../components/Select.vue';
+
+const handleSelect = function (e: string) {
+  document.querySelector('html')?.setAttribute('data-theme', e);
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@
     <header id="main-header">
       <a href="#main-content" class="skip-to-content">Skip to main content</a>
       <TopBar :isOpen="false" />
-      <TopHeader data-auto-theme="light" />
+      <TopHeader />
       <DesktopMenu isHomePage :showActiveNavigation="true" />
       <MobileMenu :showActiveNavigation="true" />
       <Breadcrumb isHomePage />
